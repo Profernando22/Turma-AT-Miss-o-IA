5e52a6e56dd8d4b86a922a6f32d0398c22328794
Profernando22 <160173688+Profernando22@users.noreply.github.com>
2024-08-30 14:06:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Na atualidade o uso do celular pode ser considerado um problema ecológico e sustentável motivado pelo  elevado consumo  e descarte irregular de aparelhos tecnológicos. Um dos motivos para este elevado número de descarte se refere a  velocidade com que as versões são lançadas no mercado e alterações ocorrem nos hardwares e processadores.  Como a IA pode contribuir para o destino correto dos descartes dos eletrônicos?",
+        enunciado: "Na atualidade o uso do celular pode ser considerado um problema ecológico e sustentável motivado pelo  elevado consumo  e descarte irregular de aparelhos tecnológicos. Um dos motivos para este elevado número de descarte se refere a velocidade com que as versões são lançadas no mercado e alterações ocorrem nos hardwares e processadores.  Como a IA pode contribuir para o destino correto dos descartes dos eletrônicos?",
         alternativas: [
             {
            Texto:"A - Oferecer mais recursos para o direcionamento dos descartes e otimização das rotas de coleta de resíduos eletrônicos.", 
             Afirmacao: "Facilitar as opções de descartes de resíduos eletrônicos.",
             },
             {
-            Texto: "B - Prever tendências de descarte com base em dados de vendas de novos modelos e padrões de consumo 2",
-            Afirmacao: "Afirmação da resposta B.",
+            Texto: "B - Prever tendências de descarte com base em dados de vendas de novos modelos e padrões de consumo",
+            Afirmacao: "Criar mecanismo de atualização de software que possam frear a moda de celulares novos",
             },
         ]
     },
@@ -27,7 +27,7 @@ const perguntas = [
             },
             {
             Texto: "B - O consumismo também pode influenciar o desenvolvimento de tecnologias de IA voltadas para  o consumo, aumentando a coleta e o processamento de recursos naturais, o que pode impactar negativamente a pegada de carbono e a sustentabilidade energética. ",
-            Afirmacao: "Conhece e prevê que o uso da.",
+            Afirmacao: "Conhece e prevê que o uso das tecnologias voltadas para IA.",
             },
         ]
     },
@@ -36,11 +36,11 @@ const perguntas = [
             alternativas: [
                 {
                 Texto: "A - A IA pode auxiliar a todos na  melhora decisão ou escolhas de respostas quanto ao desperdício e exploração de recursos naturais.", 
-                Afirmacao: "Afirmação da resposta A.",
+                Afirmacao: "Através de dados gerados pela IA é possível tomar uma decisão quanto ao desperdício, promovendo práticas sustentáveis.",
                 },
                 {
                 Texto: "B - A IA pode oferecer melhores as opções sobre os produtos mais sustentáveis ou mesmo indicando ou influenciando na escolha e compra de produtos para o consumo ecologicamente correto.",
-                Afirmacao: "Afirmação da resposta B.",
+                Afirmacao: "Indica e esclarece dúvidas sobra sustentabilidade.",
                 },
             ]
             }   
